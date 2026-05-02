1071f307df7a319094f4167fa70c6ebe19e33c86
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2026-05-02 17:28:34 +0700

    feat: init projects (#1)
    * feat: init main projects
    * test: add empty test projects
    * fix: missing update
    * chore: add initial rider settings
    * fix: align namespace
    * chore: add dotnet tool manifest
    * ci(api): add initial WiP PR CI
    * ci(api): fix dotnet-format call
    * ci(api): fix dotnet-format call v2
    * ci(api): fix dotnet-format call v3
    * ci(api): disable dotnet-format
    * ci(mcp): add initial WiP PR CI
    * ci(github): add codeql checks
    * ci(api): update trigger path rules
    * ci(github): update dependabot
    * ci(github): update codeql
    * chore: update vscode / editor settings
    * ci: remove un-used parameter

---
+++ src/Domain/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ssmsmcp.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection UseDomainLayer(
+            [NotNull] this IServiceCollection services
+            )
+    {
+        return services;
+    }
+}
---
+++ tests/Application.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ssmsmcp.Application.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
